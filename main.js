b1d4a10f607a768df5e04d079242552868e3e4c8
newsty <xadichaermatova19@gmail.com>
2023-06-18 20:53:24 +0500

    m

---
+++ main.js
@@ -0,0 +1,442 @@
+        //   1-vazifa
+
+
+
+// 1-masala
+// let num=40;
+// function musbat(){
+//     if(num>0){
+//         console.log(true);
+//     }
+//     else if(num<0){
+//         console.log(false);
+//     }
+//     else{
+//         console.log('0 ga teng');
+//     }
+// }
+// musbat(num)
+
+// 2-masala
+// let num=20;
+// function toq(){
+//     if(num % 2 !=0){
+//         console.log(true);
+//     }else {
+//         console.log(false);
+//     }
+// }
+// toq(num)
+
+// 3-masala
+// let num=25;
+// function juft(){
+//     if(num % 2 ==0){
+//         console.log(true);
+//     }else{
+//         console.log(false);
+//     }
+// }
+// juft(num)
+
+// 4-masala
+// function son(a,b){
+//     if(a>0 && b>0){
+//         console.log(true);
+//     }else{
+//         console.log(false);
+//     }
+// }
+// son(2,-7)
+
+// 5-masala
+// function son(a,b,c){
+//     if(a>b && b>c){
+//         console.log(true);
+//     }else{
+//         console.log(false);
+//     }
+// }
+// son(2,4,1)
+
+// 6-masala
+// function son(a,b,c){
+//     if(b>a && b<c || b<a && b>c){
+//         console.log(true);
+//     }else{
+//         console.log(false);
+//     }
+// }
+// son(1,8,9)
+
+// 7-masala
+// function son(a,b){
+//     if(a %2 !=0 && b %2 !=0){
+//         console.log(true);
+//     }else{
+//         console.log(false);
+//     }
+// }
+// son(2,-7)
+
+// 8-masala
+// function son(a,b,c,d){
+//     if(a>0 && b>0 && c>0 && d>0){
+//         console.log(true);
+//     }else{
+//         console.log(false);
+//     }
+// }
+// son(4,8,9,-7)
+
+// 9-masala
+// function son(a,b,c){
+//     if(a>0 && b>0 || a>0 && c>0 || b>0 && c>0){
+//         console.log(true);
+//     }else{
+//         console.log(false);
+//     }
+// }
+// son(-7,8,1)
+
+// 10-masala
+// let day = 1;
+// function week() {
+//   switch (day) {
+//     case 1:
+//       console.log("Monday");
+//       break;
+//     case 2:
+//       console.log("Tuesday");
+//       break;
+//     case 3:
+//       console.log("Wednesday");
+//       break;
+//     case 4:
+//       console.log("Thusday");
+//       break;
+//     case 5:
+//       console.log("Friday");
+//       break;
+//     case 6:
+//       console.log("Saturday");
+//       break;
+//     case 7:
+//       console.log("Sunday");
+//       break;
+//     default:
+//     console.log("bunday kun mavjud emas");
+//   }
+// }
+// week()
+
+
+// 11-masala
+// let num=90;
+// function tel(){
+//    switch (num){
+//     case 90:
+//         console.log('BELINE')
+//         break;
+//     case 94:
+//         console.log('USEL')
+//         break;
+//     case 93:
+//         console.log('UMS')
+//         break;  
+//       default:
+//            console.log("Bunday raqam mavjud emas"); 
+//    }
+// }
+// tel()
+ 
+
+// 12-masala
+// function son(num){
+//     if(num >0){
+//         num++
+//         console.log(num);
+//     }else if(num<0){
+//         num--
+//         console.log(num);
+//     }
+// }
+// son(-8)
+
+
+// 13-masala
+// function son(num){
+//     if(num >0){
+//         num=num+3
+//         console.log(num);
+//     }else if(num<0){
+//         num=num-2
+//         console.log(num);
+//     }
+// }
+// son(-7)
+
+// 14-masala
+// function son(a,b){
+//     if(a>b){
+//         console.log('a katta son');
+//     }else if(b>a){
+//         console.log('b katta son');
+//     }
+// }
+// son(4,5)
+
+// 15-masala
+// function son(a,b,c){
+//     if(a>b && a>c){
+//         console.log('a katta son');
+//     }else if(b>a && b>c){
+//         console.log('b katta son');
+//     }else if(c>a && c>b){
+//         console.log('c katta son');
+//     }
+
+// }
+// son(9,5,7)
+
+// 16-masala
+// function son(a,b,c){
+//     if(a<b && a<c){
+//         console.log('a kichik son');
+//     }else if(b<a && b<c){
+//         console.log('b kichik son');
+//     }else if(c<a && c<b){
+//         console.log('c kichik son');
+//     }
+
+// }
+// son(9,15,7)
+
+// 18-masala
+// let ball=5;
+// function stipendiya(ball){
+//     switch(ball){
+//         case 5:
+//             console.log(" sizning stipendiyangiz 900 000");
+//             break;
+//        case 4:
+//             console.log(" sizning stipendiyangiz 700 000");
+//             break; 
+//        case 3:
+//             console.log(" sizning stipendiyangiz 500 000");
+//             break;
+//             default:
+//                 console.log(' stipendiya olish uchun balingiz yetarli emas');         
+//     }
+// }
+// stipendiya(ball)
+
+// 19-masala
+// function son(a){
+//     if(a>0 && a%2 !=0 ){
+//         console.log('toq musbat');
+//     }else if(a>0 && a%2 ==0 ){
+//         console.log('juft musbat');
+//     }
+//     else if(a<0 && a%2 !=0 ){
+//         console.log('toq manfiy');
+//     }else if(a<0 && a%2 ==0 ){
+//         console.log('juft manfiy');
+//     }
+// }
+// son(-8)
+
+// 20-masala
+// var year=2023;
+// var person=1992;
+// function age(person){
+//  var res=year-person 
+//  console.log(res); 
+// }
+// age(person)
+
+
+
+        // 2-vazifa  Switch case 
+
+// 1-masala
+// let day = 1;
+// function week() {
+//   switch (day) {
+//     case 1:
+//       console.log("Monday");
+//       break;
+//     case 2:
+//       console.log("Tuesday");
+//       break;
+//     case 3:
+//       console.log("Wednesday");
+//       break;
+//     case 4:
+//       console.log("Thusday");
+//       break;
+//     case 5:
+//       console.log("Friday");
+//       break;
+//     case 6:
+//       console.log("Saturday");
+//       break;
+//     case 7:
+//       console.log("Sunday");
+//       break;
+//     default:
+//     console.log("bunday kun mavjud emas");
+//   }
+// }
+// week()
+
+// 2-masala
+// let k=4;
+// function ball(k){
+//     switch(k){
+//         case 5:
+//             console.log("alo");
+//             break;
+//        case 4:
+//             console.log("yaxshi");
+//             break; 
+//        case 3:
+//             console.log("qoniqarli");
+//             break; 
+//        case 2:
+//             console.log("qoniqarsiz");
+//             break;  
+//        case 1:
+//             console.log("yomon");
+//             break; 
+//       default:
+//         console.log("mavjud bo'lmagan raqam kiritdingiz");                           
+//     }
+// }
+// ball(k)
+
+// 3-masala
+// let num=11;
+// function season(num){
+//     switch(num){
+//         case 12 :
+//         case 1:
+//         case 2:
+               
+//             console.log('winter');
+//             break;
+//         case 3:
+//         case 4:
+//         case 5:        
+//             console.log('spring');
+//             break;
+//         case 6:
+//         case 7:
+//         case 8:        
+//             console.log('summer');
+//             break;
+//         case 9:
+//         case 10:
+//         case 11:        
+//             console.log('autumn');
+//             break;  
+//      default:
+//         console.log('bunday oy mavjud emas');                          
+//     }
+// }
+// season(num)
+
+// 4-masala
+// let moon=4;
+// function day(moon){
+//     switch (moon){
+//         case 1:
+//             console.log('january 31 day')
+//             break;
+//         case 2:
+//             console.log('february 28 day')
+//             break;
+//         case 3:
+//             console.log('march 31 day')
+//             break; 
+//         case 4:
+//             console.log( 'april 30 day')
+//             break; 
+//         case 5:
+//             console.log('may 31 day')
+//             break;
+//         case 6:
+//             console.log('june 30 day')
+//             break;
+//         case 7:
+//             console.log('july 31 day')
+//             break;  
+//         case 8:
+//             console.log('august 31 day')
+//             break;
+//         case 9:
+//             console.log('september 30 day')
+//             break; 
+//         case 10:
+//             console.log('october 31 day')
+//             break; 
+//         case 11:
+//             console.log( 'november 30 day')
+//             break; 
+//         case 12:
+//             console.log('december 31 day')
+//             break;                                           
+//         default:
+//             console.log('bunday oy mavjud emas');
+//     }
+// }
+// day(moon)
+
+// 5-masala
+//  tushunmadim ???
+
+
+
+
+
+// 6-masala
+// let ball=5;
+// function stipendiya(ball){
+//     switch(ball){
+//         case 5:
+//             console.log(" sizning stipendiyangiz 900 000");
+//             break;
+//        case 4:
+//             console.log(" sizning stipendiyangiz 700 000");
+//             break; 
+//        case 3:
+//             console.log(" sizning stipendiyangiz 500 000");
+//             break;
+//             default:
+//                 console.log(' stipendiya olish uchun balingiz yetarli emas');         
+//     }
+// }
+// stipendiya(ball)
+
+
+
+
+
+
+            //  3-vazifa   For syntax
+
+// 1-masala
+// let k=8,n=5;
+// for(let i=0;i<n;i++){
+//     console.log(k);
+// }
+
+// 2-masala
+// let a=21, b=8;
+// for(let i=b;i<=a;i++){
+//     console.log(i); 
+// }
+
+
+
+
